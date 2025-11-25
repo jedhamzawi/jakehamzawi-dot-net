e6ae915e00a2f22f0ed82232bfcfa539d393b0a6
Jake Hamzawi <mail@jakehamzawi.net>
2025-11-24 16:42:44 -0800

    Removed blog and template placeholders

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const MY_NAME = "Jake Hamzawi";
+export const GITHUB = "https://github.com/jedhamzawi"
+export const SITE_TITLE = "Jake Hamzawi";
+export const SITE_DESCRIPTION = "Jake Hamzawi's personal website";
